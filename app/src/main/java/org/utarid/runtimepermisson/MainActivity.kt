4d2cd45f0d263eaf1c43d6d5df0b70d7a6e3ab6a
utarid <utarid@protonmail.com>
2021-07-24 23:12:02 +0300

    view binding

--- app/src/main/java/org/utarid/runtimepermisson/MainActivity.kt
+++ app/src/main/java/org/utarid/runtimepermisson/MainActivity.kt
@@ -2,10 +2,26 @@ package org.utarid.runtimepermisson
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import org.utarid.runtimepermisson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view: View = binding!!.root
+        setContentView(view.rootView)
+
+        binding!!.btnRequestPermisson.setOnClickListener {
+            requestPerm()
+        }
+    }
+
+    private fun requestPerm() {
+
     }
 }
