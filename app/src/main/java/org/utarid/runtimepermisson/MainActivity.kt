608495a25b89d10658cda7b1545c7e128f6f8f18
utarid <utarid@protonmail.com>
2021-07-25 00:36:50 +0300

    add request runtime permission

--- app/src/main/java/org/utarid/runtimepermisson/MainActivity.kt
+++ app/src/main/java/org/utarid/runtimepermisson/MainActivity.kt
@@ -1,12 +1,17 @@
 package org.utarid.runtimepermisson
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import androidx.core.content.ContextCompat
 import org.utarid.runtimepermisson.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val REQUEST_CODE: Int = 1
     private var binding: ActivityMainBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +27,39 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestPerm() {
+        if (ContextCompat.checkSelfPermission(
+                applicationContext,
+                Manifest.permission.READ_CONTACTS
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            // user has already given permission
+        } else {
+            // ask for the permission.
+            requestPermissions(
+                arrayOf(Manifest.permission.READ_CONTACTS),
+                REQUEST_CODE
+            )
+        }
+    }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            REQUEST_CODE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                ) {
+                    // Permission is granted. Continue the action or workflow in your app.
+                } else {
+                    // user denied permission
+                }
+                return
+            }
+        }
     }
 }
